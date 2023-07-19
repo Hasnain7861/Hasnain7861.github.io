374d6515b8032a5bb0cfd9355f62adfbf6b04dfe
Hasnain Mucklai <mucklaihasnain@gmail.com>
2023-07-19 15:06:47 -0700

    Added pictures to potfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/sjsu.jpg'
+import GPT_Logo from '../../assets/Chat_GPT_Clone.png'
+import messaging_Logo from '../../assets/messaging_app_logo.jpg'
+import Recipe_Logo from '../../assets/Recipe_App_Image.jpg'
 
 const Portfolio = () => {
   return (
@@ -8,31 +11,31 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className="containre portfolio__container">
+      <div className="container portfolio__container">
         <article className="portfolio__item">
-          < div classname="portfolio__item.image">
-            <img src={IMG1} alt="" />
-            <h3>This is a portfolio item title</h3>
+          < div className="portfolio__item.image">
+            <img src={GPT_Logo} alt="" />
+            <h3>ChatGPT Clone</h3>
 
-            <a href="https://github.com" target="_blank" className='btn'>Github</a>
+            <a href="https://github.com/Hasnain7861/Chat-GPT-Clone" target="_blank" className='btn'>Github</a>
           </div>
         </article>
 
         <article className="portfolio__item">
-          < div classname="portfolio__item.image">
-            <img src={IMG1} alt="" />
-            <h3>This is a portfolio item title</h3>
+          < div className="portfolio__item.image">
+            <img src={Recipe_Logo} alt="" />
+            <h3>Recipe App</h3>
 
-            <a href="https://github.com" target="_blank"className='btn'>Github</a>
+            <a href="https://github.com/Hasnain7861/Recipe_App" target="_blank"className='btn'>Github</a>
           </div>
         </article>
 
         <article className="portfolio__item">
-          < div classname="portfolio__item.image">
-            <img src={IMG1} alt="" />
-            <h3>This is a portfolio item title</h3>
+          < div className="portfolio__item.image">
+            <img src={messaging_Logo} alt="" />
+            <h3>Social Media Webpage</h3>
 
-            <a href="https://github.com" target="_blank" className='btn'>Github</a>
+            <a href="https://github.com/neelp03/Team1" target="_blank" className='btn'>Github</a>
           </div>
         </article>
       </div>
